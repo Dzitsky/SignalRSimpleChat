6cb4eaa9b2cc5297b2b83d647e6a7369e8bf3444
Automerge <automerge@rts-tender.ru>
2023-05-19 19:12:41 +0300

    Commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using SignalRSimpleChat;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,11 +16,12 @@ if (!app.Environment.IsDevelopment())
 {
 	app.UseExceptionHandler("/Error");
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+	//app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
 
 app.UseRouting();
 
